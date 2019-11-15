20a5f292acf93d68925e4fb5e48ef579c83b3ea9
sagarsv04 <scoutsv.sv@gmail.com>
2019-11-15 13:50:42 -0500

    - done with dummy code up to rest run

--- src/coursing.py
+++ src/coursing.py
@@ -3,8 +3,10 @@
 Script to chase an object using astra camera sensors - Assignment 4
 """
 # Import ROS libraries and messages
+from __future__ import print_function
 import rospy
 from sensor_msgs.msg import Image
+from collections import deque
 
 # Import OpenCV libraries and tools
 import cv2
@@ -19,6 +21,8 @@ RED_LOWER_BOUNDS_0 = (0, 100, 100)
 RED_UPPER_BOUNDS_0 = (10, 255, 255)
 RED_LOWER_BOUNDS_1 = (160, 100, 100)
 RED_UPPER_BOUNDS_1 = (180, 255, 255)
+POINTS = deque(maxlen=32)
+COUNTER = 0
 
 CIRCLE_RADIUS = 2
 
@@ -27,28 +31,21 @@ def show_image(img):
   cv2.imshow("Image Window", img)
   cv2.waitKey(3)
 
-# Define a callback for the Image message
-def image_callback(img_msg, bridge):
-  # log some info about the image topic
-  rospy.loginfo(img_msg.header)
 
-  # Try to convert the ROS Image message to a CV2 Image
-  try:
-	  cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
-  except CvBridgeError, e:
-	  rospy.logerr("CvBridge Error: {0}".format(e))
+def draw_hough_circle(dilated_mask):
+	# red_mask = cv2.erode(red_mask, None, iterations=2)
+	# red_mask = cv2.dilate(red_mask, None, iterations=2)
+	detected_circles = cv2.HoughCircles(dilated_mask, cv2.HOUGH_GRADIENT, 1, 150, param1=100, param2=20, minRadius=10, maxRadius=20)
 
-  # Flip the image 90deg
-  cv_image = cv2.transpose(cv_image)
-  cv_image = cv2.flip(cv_image,1)
-  print "IMAGE DATA \n"
-  print cv_image, "\n"
-  time.sleep(2)
+	if detected_circles is not None:
+		for circle in detected_circles[0, :]:
+			circled_orig = cv2.circle(frame, (circle[0], circle[1]), circle[2], (0,255,0),thickness=3)
+		return True, circled_orig
+	else:
+		return False, dilated_mask
 
-  # Show the converted image
-  # show_image(cv_image)
 
-def draw_circle(frame):
+def process_rgb_frame(frame):
 	# blusing the image to reduce noise
 	blurred_frame = cv2.GaussianBlur(frame, (9, 9),3,3)
 	# converting the input stream into HSV color space
@@ -66,102 +63,179 @@ def draw_circle(frame):
 	eroded_mask = cv2.erode(weighted_mask,erode_element)
 	dilated_mask = cv2.dilate(eroded_mask,dilate_element)
 
-	# red_mask = cv2.erode(red_mask, None, iterations=2)
-	# red_mask = cv2.dilate(red_mask, None, iterations=2)
-	detected_circles = cv2.HoughCircles(dilated_mask, cv2.HOUGH_GRADIENT, 1, 150, param1=100, param2=20, minRadius=10, maxRadius=200)
+	return dilated_mask
 
-	if detected_circles is not None:
-		for circle in detected_circles[0, :]:
-			circled_orig = cv2.circle(frame, (circle[0], circle[1]), circle[2], (0,255,0),thickness=3)
-		return circled_orig
-	else:
-		return frame
+
+def compute_the_contour(dilated_mask, frame):
+	global POINTS
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(dilated_mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	# update the points queue
+	POINTS.appendleft(center)
+	# loop over the set of tracked points
+	for i in range(1, len(POINTS)):
+		# if either of the tracked points are None, ignore
+		# them
+		if POINTS[i - 1] is None or POINTS[i] is None:
+			continue
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		cv2.line(frame, POINTS[i - 1], POINTS[i], (0, 0, 255), 3)
+
+	return frame
 
 
 def red_circle_image():
 	# read image into rgb frame
 	frame = cv2.imread('./res/red_circle.jpg')
-	frame_ = draw_circle(frame)
+	dilated_mask = process_rgb_frame(frame)
+	flag, circled_orig = draw_hough_circle(dilated_mask)
+	frame = compute_the_contour(dilated_mask, frame)
 	cnts = cv2.findContours(red_mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	center = None
 
-
-def opencv_test():
-	cap = cv2.VideoCapture(0)
+	return 0
 
 
+def get_motion_command(direction):
+	move_msg = Twist()
+
+	direction.replace("north","front")
+	direction.replace("east","right")
+	direction.replace("west","left")
+	direction.replace("south","back")
+
+	if motion_dict == "left":
+		move_msg.linear.x = 0
+		move_msg.angular.z = 0.3
+	elif motion_dict == "right":
+		move_msg.linear.x = 0
+		move_msg.angular.z = -0.3
+	elif motion_dict == "front":
+		move_msg.linear.x = 0.4
+		move_msg.angular.z = 0
+	elif motion_dict == "back":
+		move_msg.linear.x = -0.4
+		move_msg.angular.z = 0
+	elif motion_dict == "front-right":
+		move_msg.linear.x = 0.4
+		move_msg.angular.z = -0.3
+	elif motion_dict == "front-left":
+		move_msg.linear.x = 0.4
+		move_msg.angular.z = 0.3
+	elif motion_dict == "back-right":
+		move_msg.linear.x = -0.4
+		move_msg.angular.z = -0.3
+	elif motion_dict == "back-left":
+		move_msg.linear.x = -0.4
+		move_msg.angular.z = 0.3
+	else:
+		move_msg.linear.x = 0
+		move_msg.angular.z = 0
+		print("Not Moving\n")
+		return move_msg, False
+
+	return move_msg, True
 
 
-	# some morphological operations (closing) to remove small blobs
-	# erode_element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-	# dilate_element = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
-	# eroded_mask = cv2.erode(red_mask,erode_element)
-	# dilated_mask = cv2.dilate(eroded_mask,dilate_element)
+def track_movement(frame):
+	direction = ""
+	# loop over the set of tracked points
+	for i in np.arange(1, len(POINTS)):
+		# if either of the tracked points are None, ignore
+		# them
+		if POINTS[i - 1] is None or POINTS[i] is None:
+			continue
+		# check to see if enough points have been accumulated in
+		# the buffer
+		if COUNTER >= 10 and i == 1 and POINTS[-10] is not None:
+			# compute the difference between the x and y
+			# coordinates and re-initialize the direction
+			# text variables
+			dX = POINTS[-10][0] - POINTS[i][0]
+			dY = POINTS[-10][1] - POINTS[i][1]
+			dirX, dirY = "", ""
+			# ensure there is significant movement in the
+			# x-direction
+			if np.abs(dX) > 20:
+				dirX = "east" if np.sign(dX) == 1 else "west"
 
-	# on the color-masked, blurred and morphed image I apply the cv2.HoughCircles-method to detect circle-shaped objects
-	# detected_circles = cv2.HoughCircles(dilated_mask, cv2.HOUGH_GRADIENT, 1, 150, param1=100, param2=20, minRadius=0, maxRadius=200)
+			# ensure there is significant movement in the
+			# y-direction
+			if np.abs(dY) > 20:
+				dirY = "north" if np.sign(dY) == 1 else "south"
 
-
-
-
-	#
-	# bright_red_lower_bounds = (0, 100, 100)
-	# bright_red_upper_bounds = (10, 255, 255)
-	#
-	# v_frame, s_frame, h_frame  = cv2.split(hsv_frame)
-	#
-	# hsv_conv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	#
-	# # ret, h_frame_thresh_abv = cv2.threshold(h_frame, 70, 255, cv2.THRESH_BINARY_INV)
-	# # ret, h_frame_thresh_abv = cv2.threshold(h_frame, 70, 255, cv2.THRESH_BINARY)
-	#
-	# # h_frame_thresh_abv = cv2.inRange(h_frame, 60, 70)
-	# # s_frame_thresh_abv = cv2.inRange(s_frame, 60, 70)
-	# v_frame_thresh_abv = cv2.inRange(v_frame, 60, 70)
-	#
-
-	# cv2.imwrite('./res/red_circle_h.jpg', h_frame)
-	# cv2.imwrite('./res/red_circle_s.jpg', s_frame)
-	# cv2.imwrite('./res/red_circle_v.jpg', v_frame)
-
-	while(True):
-		# Capture frame-by-frame
-		ret, frame = cap.read()
-
-
-		frame_ = draw_circle(frame)
-		# cv2.imshow('frame',dark_red_mask)
-		# cv2.imshow('frame',s_frame)
-		# cv2.imshow('frame',h_frame_thresh_abv)
-		# cv2.imshow('frame',s_frame_thresh_abv)
-		# cv2.imshow('frame',v_frame_thresh_abv)
-		cv2.imshow('frame',frame_)
+			# handle when both directions are non-empty
+			if dirX != "" and dirY != "":
+				direction = "{}-{}".format(dirY, dirX)
+			# otherwise, only one direction is non-empty
+			elif dirX == "" and dirY != "":
+				direction = dirY
+			elif dirY == "" and dirX != "":
+				direction = dirX
+			else:
+				direction = "nowhere"
+	cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 3)
+	print("D--->\n",direction)
+	return direction
 
 
-		# Our operations on the frame come here
-		# gray = cv2.transpose(gray)
-		# gray = cv2.flip(gray,1)
+# Define a callback for the Image message
+def image_callback(img_msg, bridge, motion_pub):
+	global COUNTER
+	# log some info about the image topic
+	rospy.loginfo(img_msg.header)
 
-		# Display the resulting frame
-		# print "IMAGE\n"
-		# print frame.shape
-		# print gray,"\n"
-		# time.sleep(0.5)
-		# cv2.imshow('frame',gray)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+	# Try to convert the ROS Image message to a CV2 Image
+	try:
+		cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+	except CvBridgeError, e:
+		rospy.logerr("CvBridge Error: {0}".format(e))
 
-	# When everything done, release the capture
-	# cap.release()
-	cv2.destroyAllWindows()
+	# Flip the image 90deg
+	# cv_image = cv2.transpose(cv_image)
+	# cv_image = cv2.flip(cv_image,1)
+	dilated_mask = process_rgb_frame(frame)
+	# flag, circled_orig = draw_hough_circle(dilated_mask)
+	frame = compute_the_contour(dilated_mask, frame)
+	direction = track_movement(frame)
+	move_msg, move_flag = get_motion_command(direction)
 
+	if move_flag:
+		COUNTER = 0
+		motion_pub.publish(move_msg)
+	else:
+		print("Nothing to Publish")
+
+	return 0
 
 
 def run_ros():
 	rospy.init_node('coursing', anonymous=False)
+	motion_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
 	bridge = CvBridge() # Initialize the CvBridge class
-	callback_lambda = lambda x: image_callback(x,bridge)
+	callback_lambda = lambda x: image_callback(x, bridge, motion_pub)
 	sub_image = rospy.Subscriber("/camera/rgb/image_raw", Image, callback_lambda, queue_size=1)
 	# cv2.namedWindow("Image Window", 1)
 	while not rospy.is_shutdown():
@@ -171,7 +245,8 @@ def run_ros():
 
 
 def main():
-	opencv_test()
+	# opencv_test()
+	run_ros()
 	return 0
 
 
